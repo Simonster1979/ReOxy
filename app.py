3a4656a76f5b460c3d23fa6d332e00acdac4b679
shery909 <muhammad.shaheryar@supersight.fi>
2025-04-06 02:15:42 +0300

    y = 2 x = 6

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ def extract_text_from_pdf(pdf_file):
         
         for page in pdf.pages:
             words = page.extract_words(
-                x_tolerance=5,
-                y_tolerance=5,
+                x_tolerance=6,
+                y_tolerance=2,
                 keep_blank_chars=True,
                 use_text_flow=True
             )
